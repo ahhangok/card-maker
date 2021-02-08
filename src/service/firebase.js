40490c084b78ae6e5f036be2420a6885f41340f3
JM <dlwlals0406@naver.com>
2021-02-08 10:29:20 +0900

    project set up & add firebase

---
+++ src/service/firebase.js
@@ -0,0 +1,9 @@
+   const firebaseConfig = {
+    apiKey: "AIzaSyAr906Nr3PETiYPCCAQw-NK41PzlAHf5no",
+    authDomain: "business-card-maker-ad1f9.firebaseapp.com",
+    projectId: "business-card-maker-ad1f9",
+    databaseURL: "http://business-card-maker-ad1f9.firebaseio.com",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
